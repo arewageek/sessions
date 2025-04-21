de48340c12740d4bfa4d50a94e3d9f266c6ecfe5
Austin Ameh <arewageek@gmail.com>
2025-04-21 04:16:11 +0100

    First local and testnet deployment, update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    testnet: {
+      // base sepolia
+      url: "https://base-sepolia.infura.io/v3/918b4f4db0304000a95f1840cb1de66a",
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    mainnet: {
+      url: "https://base-mainnet.infura.io/v3/918b4f4db0304000a95f1840cb1de66a",
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
--- ignition/modules/Sessions.ts
+++ ignition/modules/Sessions.ts
@@ -1,7 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const SessionsModule = buildModule("SessionsModule", (m) => {
-  const sessions = m.contract("Sessions");
+  const sessions = m.contract("Sessions", [
+    "0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70", // chainlink's price feed CA for ETH/USD on base network
+  ]);
 
   return { sessions };
 });
