d108828ff8ffe0248fe90b12681731dbfb618d0a
Austin Ameh <arewageek@gmail.com>
2025-04-30 22:02:30 +0100

    Update contract deployment script, Redeploy contract,

--- ignition/modules/Sessions.ts
+++ ignition/modules/Sessions.ts
@@ -1,9 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const SessionsModule = buildModule("SessionsModule", (m) => {
-  const sessions = m.contract("Sessions", [
-    "0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70", // chainlink's price feed CA for ETH/USD on base network
-  ]);
+  const sessions = m.contract("Sessions", []);
 
   return { sessions };
 });
